77d18e0a151e542188194bc0307ea04bff6a1324
Matt Gowie <gowie.matt@gmail.com>
2017-02-07 11:24:17 -0500

    Adds /last_location endpoint

--- lib/router.ex
+++ lib/router.ex
@@ -18,6 +18,19 @@ defmodule WhereAmIBackend.Router do
  end
 
   @doc """
+  /last_location
+
+  An endpoint to fetch the last location pushed onto the 'locations' REDIS list.
+  Returns the full JSON payload of that last entry.
+  """
+  get "/last_location" do
+    {:ok, location_json } = :poolboy.transaction(:redis_pool, fn(worker) -> Redix.command(worker, ~w(LINDEX locations -1)) end)
+    conn = conn |> Plug.Conn.put_resp_header("content-type", "application/json")
+                |> Plug.Conn.put_resp_header("Access-Control-Allow-Origin", "https://mattgowie.com")
+    send_resp(conn, 200, location_json)
+  end
+
+  @doc """
   /location
 
   An endpoint to allowe the where_am_i Elixir command-line client to upload
